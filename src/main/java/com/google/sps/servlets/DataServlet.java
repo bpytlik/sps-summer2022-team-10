8a25fbabaeef9bec141cc9b1f6e615ba2b8ecfa7
Irene <irenec20152016@gmail.com>
2022-07-05 06:32:44 +0000

    add data servlet - first commit

---
+++ src/main/java/com/google/sps/servlets/DataServlet.java
@@ -0,0 +1,18 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Handles Kroger API, send requests to API and get the data  */
+@WebServlet("/data-kroger")
+public class DataServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html;");    
+    response.getWriter().println("<h1>Kroger Data</h1>");
+  }
+}
