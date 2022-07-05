572fc0de9eb997dcd8f0fd77f411e3861fc5e6a7
Tianyu Chen <tianyuchen1016@gmail.com>
2022-07-05 21:54:00 +0000

    add basic structure of loadTable to price comparison page

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -26,3 +26,55 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+async function loadTable() {
+    // const responseFromServer = await fetch('/data-processing');
+    // const data = await responseFromServer.json();
+
+    // hardcoded for now
+    const response = '{"stores": ["A", "B", "C"], "items": ["milk", "egg", "apple"], "prices": [[1, 2, 3], [4, 5, 6], [7, 8, 9]], "totals": [12, 15, 18]}';
+    const data = JSON.parse(response);
+
+    const dataTable = document.getElementById('data-table');
+
+    for (let r = 0; r < data.items.length + 2; r++) {
+        var rowElement = document.createElement('tr');
+        for (let c = 0; c < data.stores.length + 1; c++) {
+            if (c == 0 && r == 0) {
+                const cell = document.createElement('th');
+                cell.appendChild(document.createTextNode("Item"));
+                rowElement.appendChild(cell);
+            }
+            else if (c == 0 && r == data.items.length + 1) {
+                const cell = document.createElement('th');
+                cell.appendChild(document.createTextNode("Total"));
+                rowElement.appendChild(cell);
+            }
+            // handle store names
+            else if (r == 0) {
+                const cell = document.createElement('th');
+                cell.appendChild(document.createTextNode(data.stores[c-1]));
+                rowElement.appendChild(cell);
+            }
+            // handle item names
+            else if (c == 0) {
+                const cell = document.createElement('th');
+                cell.appendChild(document.createTextNode(data.items[r-1]));
+                rowElement.appendChild(cell);
+            }
+            // handle totals
+            else if (r == data.items.length + 1) {
+                const cell = document.createElement('th');
+                cell.appendChild(document.createTextNode(data.totals[c-1]));
+                rowElement.appendChild(cell);
+            }
+            // handle individual prices
+            else {
+                const cell = document.createElement('td');
+                cell.appendChild(document.createTextNode(data.prices[r-1][c-1]));
+                rowElement.appendChild(cell);
+            }
+        }
+        dataTable.appendChild(rowElement);
+    }
+}
