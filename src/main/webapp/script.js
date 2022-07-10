b05683d33ee05c72f6bb247d9f5d320c0a799acb
Tianyu Chen <tianyuchen1016@gmail.com>
2022-07-10 03:45:34 +0000

    restore calculate() and new loadTable(totals) in script.js

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -29,55 +29,43 @@ async function addToCart(){
     console.log(itemList);
 }
 
-async function loadTable() {
-    // const responseFromServer = await fetch('/data-processing');
-    // const data = await responseFromServer.json();
-
-    // hardcoded for now
-    const response = '{"stores": ["A", "B", "C"], "items": ["milk", "egg", "apple"], "prices": [[1, 2, 3], [4, 5, 6], [7, 8, 9]], "totals": [12, 15, 18]}';
-    const data = JSON.parse(response);
-
-    const dataTable = document.getElementById('data-table');
-
-    for (let r = 0; r < data.items.length + 2; r++) {
-        var rowElement = document.createElement('tr');
-        for (let c = 0; c < data.stores.length + 1; c++) {
-            if (c == 0 && r == 0) {
-                const cell = document.createElement('th');
-                cell.appendChild(document.createTextNode("Item"));
-                rowElement.appendChild(cell);
-            }
-            else if (c == 0 && r == data.items.length + 1) {
-                const cell = document.createElement('th');
-                cell.appendChild(document.createTextNode("Total"));
-                rowElement.appendChild(cell);
-            }
-            // handle store names
-            else if (r == 0) {
-                const cell = document.createElement('th');
-                cell.appendChild(document.createTextNode(data.stores[c-1]));
-                rowElement.appendChild(cell);
-            }
-            // handle item names
-            else if (c == 0) {
-                const cell = document.createElement('th');
-                cell.appendChild(document.createTextNode(data.items[r-1]));
-                rowElement.appendChild(cell);
-            }
-            // handle totals
-            else if (r == data.items.length + 1) {
-                const cell = document.createElement('th');
-                cell.appendChild(document.createTextNode(data.totals[c-1]));
-                rowElement.appendChild(cell);
-            }
-            // handle individual prices
-            else {
-                const cell = document.createElement('td');
-                cell.appendChild(document.createTextNode(data.prices[r-1][c-1]));
-                rowElement.appendChild(cell);
-            }
-        }
-        dataTable.appendChild(rowElement);
-    }
+async function calculate() {
+    // Calculate totals here ...
+    const totals = [0, 0, 0];  // change this with real totals
+    loadTable(totals);
 }
 
+async function loadTable(totals) {
+    const compTable = document.getElementById('comparison-table');
+
+    // adding the first row which shows store names
+    const rowStores = document.createElement('tr');
+    for (let i = 0; i < stores.length; i++) {
+        const cell = document.createElement('th');
+        cell.appendChild(document.createTextNode(stores[i]));
+        rowStores.appendChild(cell);
+    }
+    compTable.appendChild(rowStores);
+
+    // add following rows to show item and price in each store
+    for (let i = 0; i < itemList.length; i++) {
+        const rowItem = document.createElement('tr');
+        // get info of one shopping item in all stores
+        const dataArr = Object.values(itemList[i])[0];
+        for (let j = 0; j < dataArr.length; j++) {
+            const cell = document.createElement('td');
+            cell.appendChild(document.createTextNode(dataArr[j].item + " $" + dataArr[j].price));
+            rowItem.appendChild(cell);
+        }
+        compTable.appendChild(rowItem);
+    }
+
+    // add the last row to show total prices in each store
+    const rowTotals = document.createElement('tr');
+    for (let i = 0; i < totals.length; i++) {
+        const cell = document.createElement('th');
+        cell.appendChild(document.createTextNode("Total: $" + totals[i]));
+        rowTotals.appendChild(cell);
+    }
+    compTable.appendChild(rowTotals);
+}
